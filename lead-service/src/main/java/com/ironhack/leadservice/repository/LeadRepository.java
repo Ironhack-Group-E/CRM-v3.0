208999086c0fcca3728999a2851ee1a858821f80
Alejandro <amnaredo@gmail.com>
2021-02-19 20:39:31 +0100

    Commented out repository methods. added bootstrap dependency in pom.xml

--- lead-service/src/main/java/com/ironhack/leadservice/repository/LeadRepository.java
+++ lead-service/src/main/java/com/ironhack/leadservice/repository/LeadRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Repository
 public interface LeadRepository extends JpaRepository<Lead, Integer> {
 
-    //  Returns a list of names and count of all Leads by SalesRep
-    @Query(value = "SELECT s.name, COUNT(*) FROM Lead l JOIN FETCH SalesRep s ON l.salesRep = s.id GROUP BY s.name")
-    List<Object[]> countOfLeadsBySalesReps();
+//    //  Returns a list of names and count of all Leads by SalesRep
+//    @Query(value = "SELECT s.name, COUNT(*) FROM Lead l JOIN FETCH SalesRep s ON l.salesRep = s.id GROUP BY s.name")
+//    List<Object[]> countOfLeadsBySalesReps();
 
 
     /**
@@ -23,29 +23,29 @@ public interface LeadRepository extends JpaRepository<Lead, Integer> {
 
 
 
-    //Returns a list of products and count of all Leads by Product
-    @Query(value = "sELECT o.product, COUNT(*) FROM leads l JOIN sales_rep s ON l.sales_rep_id = s.id JOIN opportunities o ON s.id = o.sales_rep_id GROUP BY o.product;", nativeQuery = true)
-    List<Object[]> countOfLeadsByProduct();
-
-    //Return a list of Leads and count all Lead by Country
-    @Query(value = "SELECT a.country, COUNT(*) FROM accounts a " +
-            "JOIN opportunities o ON a.id = o.account_id " +
-            "JOIN sales_rep s ON s.id = o.sales_rep_id " +
-            "JOIN leads l ON s.id = l.sales_rep_id GROUP BY a.country", nativeQuery = true)
-    List<Object[]> countOfLeadsByCountry();
-
-    //Return a list of Leads and count all Lead by city
-    @Query(value = "SELECT a.city, COUNT(*) FROM accounts a " +
-            "JOIN opportunities o ON a.id = o.account_id " +
-            "JOIN sales_rep s ON s.id = o.sales_rep_id " +
-            "JOIN leads l ON s.id = l.sales_rep_id GROUP BY a.city", nativeQuery = true)
-    List<Object[]> countOfLeadsByCity();
-
-    //Return a list of Leads and count all Lead by industry
-    @Query(value = "SELECT a.industry, COUNT(*) FROM accounts a " +
-            "JOIN opportunities o ON a.id = o.account_id " +
-            "JOIN sales_rep s ON s.id = o.sales_rep_id " +
-            "JOIN leads l ON s.id = l.sales_rep_id GROUP BY a.industry", nativeQuery = true)
-    List<Object[]> countOfLeadsByIndustry();
+//    //Returns a list of products and count of all Leads by Product
+//    @Query(value = "sELECT o.product, COUNT(*) FROM leads l JOIN sales_rep s ON l.sales_rep_id = s.id JOIN opportunities o ON s.id = o.sales_rep_id GROUP BY o.product;", nativeQuery = true)
+//    List<Object[]> countOfLeadsByProduct();
+//
+//    //Return a list of Leads and count all Lead by Country
+//    @Query(value = "SELECT a.country, COUNT(*) FROM accounts a " +
+//            "JOIN opportunities o ON a.id = o.account_id " +
+//            "JOIN sales_rep s ON s.id = o.sales_rep_id " +
+//            "JOIN leads l ON s.id = l.sales_rep_id GROUP BY a.country", nativeQuery = true)
+//    List<Object[]> countOfLeadsByCountry();
+//
+//    //Return a list of Leads and count all Lead by city
+//    @Query(value = "SELECT a.city, COUNT(*) FROM accounts a " +
+//            "JOIN opportunities o ON a.id = o.account_id " +
+//            "JOIN sales_rep s ON s.id = o.sales_rep_id " +
+//            "JOIN leads l ON s.id = l.sales_rep_id GROUP BY a.city", nativeQuery = true)
+//    List<Object[]> countOfLeadsByCity();
+//
+//    //Return a list of Leads and count all Lead by industry
+//    @Query(value = "SELECT a.industry, COUNT(*) FROM accounts a " +
+//            "JOIN opportunities o ON a.id = o.account_id " +
+//            "JOIN sales_rep s ON s.id = o.sales_rep_id " +
+//            "JOIN leads l ON s.id = l.sales_rep_id GROUP BY a.industry", nativeQuery = true)
+//    List<Object[]> countOfLeadsByIndustry();
 
 }
